5fb9c7a4e13be3725dcbc22653269ccbe37543f7
Greg Albrecht <gba@gregalbrecht.com>
2013-01-10 18:38:29 -0800

    boot-strapping the javaAPRSSrvr bindings for Python.

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+__author__ = 'Greg Albrecht <gba@gregalbrecht.com>'
+__copyright__ = 'Copyright 2013 Greg Albrecht'
+__license__ = 'Creative Commons Attribution 3.0 Unported License'
+
+
+import setuptools
+
+
+setuptools.setup(
+    version='1.0.0',
+    name='aprs',
+    description='Python Bindings for aprs-is API.',
+    author='Greg Albrecht',
+    author_email='gba@gregalbrecht.com',
+    license='Creative Commons Attribution 3.0 Unported License',
+    url='https://github.com/ampledata/aprs',
+    setup_requires=['nose>=1.0']
+)
