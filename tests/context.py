e3fe434414a12e6d4df6cf741b71615f3319e325
Greg Albrecht <gba@onbeep.com>
2017-08-01 22:13:26 -0700

    more updates

--- tests/context.py
+++ tests/context.py
@@ -6,10 +6,11 @@
 import os
 import sys
 
+from . import aprs_test_classes  # NOQA pylint: disable=W0611
+
 sys.path.insert(0, os.path.abspath('..'))
 
 import aprs  # NOQA pylint: disable=W0611
-import aprs_test_classes  # NOQA pylint: disable=W0611
 
 __author__ = 'Greg Albrecht W2GMD <oss@undef.net>'  # NOQA pylint: disable=R0801
 __copyright__ = 'Copyright 2017 Greg Albrecht and Contributors'  # NOQA pylint: disable=R0801
