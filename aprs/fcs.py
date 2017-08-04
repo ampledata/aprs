542fbf090ef3ed5622fada408aa40ce7ddd99498
Greg Albrecht <gba@onbeep.com>
2017-08-03 21:56:37 -0700

    adding fcs

---
+++ aprs/fcs.py
@@ -0,0 +1,67 @@
+
+import bitarray
+
+import struct
+
+class FCS(object):
+
+    def __init__(self):
+        self.fcs = 0xFFFF
+
+    def update_bit(self, bit):
+        check = (self.fcs & 0x1 == 1)
+        self.fcs >>= 1
+        if check != bit:
+            self.fcs ^= 0x8408
+
+    def update(self, bytes):
+        for byte in (ord(b) for b in bytes):
+            for i in range(7,-1,-1):
+                self.update_bit((byte >> i) & 0x01 == 1)
+
+    def digest(self):
+#        print ~self.fcs
+#        print "%r" % struct.pack("<H", ~self.fcs % 2**16)
+#        print "%r" % "".join([chr((~self.fcs & 0xff) % 256), chr((~self.fcs >> 8) % 256)])
+        # digest is two bytes, little endian
+        return struct.pack("<H", ~self.fcs % 2**16)
+
+
+def fcs(bits):
+    '''
+    Append running bitwise FCS CRC checksum to end of generator
+    '''
+    fcs = FCS()
+    for bit in bits:
+        yield bit
+        fcs.update_bit(bit)
+
+#    test = bitarray()
+#    for byte in (digest & 0xff, digest >> 8):
+#        print byte
+#        for i in range(8):
+#            b = (byte >> i) & 1 == 1
+#            test.append(b)
+#            yield b
+
+    # append fcs digest to bit stream
+
+    # n.b. wire format is little-bit-endianness in addition to little-endian
+    digest = bitarray(endian="little")
+    digest.frombytes(fcs.digest())
+    for bit in digest:
+        yield bit
+
+def fcs_validate(bits):
+    buffer = bitarray()
+    fcs = FCS()
+
+    for bit in bits:
+        buffer.append(bit)
+        if len(buffer) > 16:
+            bit = buffer.pop(0)
+            fcs.update(bit)
+            yield bit
+
+    if buffer.tobytes() != fcs.digest():
+        raise Exception("FCS checksum invalid.")
