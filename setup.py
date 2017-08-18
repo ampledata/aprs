1b28ee715bd2ef463e0abddc57fb0676ecd83d23
Greg Albrecht <gba@onbeep.com>
2017-08-17 23:52:54 -0700

    Major Bump

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import sys
 import setuptools
 
 __title__ = 'aprs'
-__version__ = '7.0.0rc3'
+__version__ = '7.0.0'
 __author__ = 'Greg Albrecht W2GMD <oss@undef.net>'  # NOQA pylint: disable=R0801
 __copyright__ = 'Copyright 2017 Greg Albrecht and Contributors'  # NOQA pylint: disable=R0801
 __license__ = 'Apache License, Version 2.0'  # NOQA pylint: disable=R0801
